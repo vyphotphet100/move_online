f41ad30bb0c7b1466c58a40a58be6560214839b1
CAODINHSYVY <caovy2001@gmail.com>
2021-06-27 09:04:14 +0700

    change url

--- user_dashboard/mission/js/after_capcha_main.js
+++ user_dashboard/mission/js/after_capcha_main.js
@@ -1,8 +1,8 @@
-if (document.referrer == 'http://olalink.co/yDl91E') {
+if (document.referrer == 'http://olalink.co/') {
     alert('Xác nhận Capcha thành công.' + document.referrer);
 }
 else {
     alert('Xác nhận Capcha không thành công.' + document.referrer);
 }
 
-//location.href = 'https://vyphotphet100.github.io/move_online/user_dashboard/mission/index.html';
+location.href = 'https://vyphotphet100.github.io/move_online/user_dashboard/mission/index.html';

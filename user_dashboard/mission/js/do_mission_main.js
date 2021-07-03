74ac3d3835ca3ba3776ecbdf482625a49f52d9c6
vyphotphet100 <caovy2001@gmail.com>
2021-07-03 22:35:16 +0700

    done watch site

--- user_dashboard/mission/js/do_mission_main.js
+++ user_dashboard/mission/js/do_mission_main.js
@@ -11,11 +11,11 @@ function doMission() {
         return;
     }
 
-    if (missionDto.type != 'CAPCHA' &&
-        document.referrer.includes(connecter.baseUrl)) {
-        alert('Có lỗi xảy ra.');
-        return;
-    }
+    // if (missionDto.type != 'CAPCHA' &&
+    //     !document.referrer.includes(connecter.baseUrl)) {
+    //     alert('Có lỗi xảy ra.');
+    //     return;
+    // }
 
     missionDto = MissionRequest.doMission(missionId);
     alert(missionDto.message);
